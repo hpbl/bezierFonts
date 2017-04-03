eaefdde49f27f084413c917c2cbdf09eb8cb2d2e
Hilton Pintor <hpbl@cin.ufpe.br>
2017-04-03 10:53:50 -0300

    Adding how-to-play label

--- eltonPlayground.playground/Contents.swift
+++ eltonPlayground.playground/Contents.swift
@@ -5,3 +5,12 @@ import UIKit
 let eltonView = DrawingView()
 
 PlaygroundPage.current.liveView = eltonView
+
+let helpLabel = UILabel(frame: CGRect(x: 30, y: 20, width: eltonView.frame.height, height: eltonView.frame.width))
+
+helpLabel.text = "tap to add control points and form the missing letter"
+
+helpLabel.textColor = UIColor.darkGray
+helpLabel.sizeToFit()
+
+eltonView.addSubview(helpLabel)
